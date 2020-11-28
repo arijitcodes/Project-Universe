3e98d878ddeda4650ec7589f7565d52dde94ce69
Arijit Banerjee <72866688+arijitcodes@users.noreply.github.com>
2020-11-28 18:14:03 +0530

    Modify & Addition: Modified and Added 'About the Developer' section in About Page (components/pages/About.js).

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,10 +4,7 @@ import Icon from "./icon.png";
 
 const About = () => {
   return (
-    <div
-      className="container my-3 py-2"
-      style={{ backgroundColor: "rgb(0,0,0,0.7)" }}
-    >
+    <div className="my-3 py-2" style={{ backgroundColor: "rgb(0,0,0,0.7)" }}>
       <h1 align="center" className="text-light">
         PROJECT UNIVERSE
       </h1>
@@ -19,7 +16,7 @@ const About = () => {
         style={{ backgroundColor: "rgb(0,0,0,0" }}
       >
         <div className="row justify-content-center text-light">
-          <div className="col-lg-8 col-md-6 col-sm-12">
+          <div className="col-lg-8 col-md-10 col-sm-12">
             <div
               className="card border-0"
               style={{
@@ -37,7 +34,7 @@ const About = () => {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p> */}
-              <table class="table table-responsive-md text-white m-0 px-3">
+              <table className="table table-responsive-md text-white m-0 px-3">
                 {/* <thead>
                     <tr>
                       <th scope="col">#</th>
@@ -49,7 +46,7 @@ const About = () => {
                 <tbody>
                   <tr>
                     <td align="right">Project:</td>
-                    <td class="align-middle">Project Universe</td>
+                    <td className="align-middle">Project Universe</td>
                   </tr>
                   <tr>
                     <td align="right">Developer:</td>
@@ -60,16 +57,16 @@ const About = () => {
                     <td>ReactJS, NodeJS</td>
                   </tr>
                   <tr>
-                    <td align="right" class="align-baseline">
+                    <td align="right" className="align-baseline">
                       Others:
                     </td>
                     <td>Axios, React Hooks, JSX, JS, ES6, Bootstrap etc.</td>
                   </tr>
                   <tr>
-                    <td align="right" class="align-middle">
+                    <td align="right" className="align-middle">
                       Details:
                     </td>
-                    <td class="align-middle">
+                    <td className="align-middle">
                       This is a fun project I made for my fellow Space
                       Enthusiasts. The Data used in different features of this
                       app, directly comes from NASA's servers. Every day the
@@ -132,6 +129,144 @@ const About = () => {
         data-height="317"
         data-theme="medium"
       ></div> */}
+
+      <div>
+        <div className="row justify-content-center text-light">
+          <div className="col-lg-8 col-md-10 col-sm-12">
+            <div
+              className="card border-0 mx-auto"
+              style={{
+                backgroundColor: "rgba(0, 0, 0, 0)",
+                height: "100%",
+                width: "100%",
+              }}
+            >
+              <div className="card-body m-0">
+                <h5 className="card-title">About the Developer</h5>
+                <div className="row">
+                  <div className="col-lg-4 col-md-4 col-sm-12 border border-white p-0 m-0 d-flex justify-content-center align-items-center">
+                    <div id="DPandSocials">
+                      <div>
+                        <p className="card-text text-center">
+                          {/* Some quick example text to build on the card title and make up
+                  the bulk of the card's content. */}
+                          <img
+                            src="https://media-exp1.licdn.com/dms/image/C4E03AQEtdvOYW7rg4Q/profile-displayphoto-shrink_200_200/0?e=1611792000&v=beta&t=x0JQznbHpJHI2VdJ2VthlUcAJli_vuMZ-lPSAqcNuZo"
+                            className="rounded-circle m-3"
+                            alt="Icon"
+                            height="150px"
+                            width="150px"
+                          />
+                        </p>
+                      </div>
+                      <div className="d-flex justify-content-center">
+                        <a
+                          href="https://instagram.com/arijit.codes"
+                          className="fab fa-instagram custom-social mx-3 mb-2"
+                          target="_blank"
+                          rel="noreferrer"
+                          // style={{
+                          //   textDecoration: "none",
+                          //   fontSize: "25px",
+                          //   color: "white",
+                          // }}
+                        >
+                          {" "}
+                        </a>
+                        <a
+                          href="https://linkedin.com/in/arijitban"
+                          className="fab fa-linkedin custom-social mx-3 mb-2"
+                          target="_blank"
+                          rel="noreferrer"
+                          // style={{
+                          //   textDecoration: "none",
+                          //   fontSize: "25px",
+                          //   color: "white",
+                          // }}
+                        >
+                          {" "}
+                        </a>
+                        <a
+                          href="https://github.com/ArijitCodes"
+                          className="fab fa-github custom-social mx-3 mb-2"
+                          target="_blank"
+                          rel="noreferrer"
+                          // style={{
+                          //   textDecoration: "none",
+                          //   fontSize: "25px",
+                          //   color: "white",
+                          // }}
+                        >
+                          {" "}
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-8 col-md-8 col-sm-12 border border-white">
+                    <table className="table table-responsive-md table-borderless table-sm text-white m-0 px-3">
+                      <tbody>
+                        <tr>
+                          <td align="right">Name:</td>
+                          <td>Arijit Banerjee</td>
+                        </tr>
+                        <tr>
+                          <td align="right" className="align-middle">
+                            Education:
+                          </td>
+                          <td>B.Sc. with Honours in Computer Science</td>
+                        </tr>
+                        <tr>
+                          <td align="right" className="align-middle">
+                            About Me:
+                          </td>
+                          <td>Freelance Developer | Full Stack | Actor</td>
+                        </tr>
+                        <tr>
+                          <td align="right" className="align-middle">
+                            Technologies:
+                          </td>
+                          <td style={{ fontSize: "14px" }}>
+                            MERN Stack [MongoDB, ExpressJS, ReactJS, NodeJS],
+                            JavaScript ES6, HTML, Bootstrap, PHP, MySQL, Git,
+                            GitHub, Heroku, REST Api etc.
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="right" className="align-middle">
+                            Contact:
+                          </td>
+                          <td className="align-middle">
+                            arijit.codes@gmail.com
+                          </td>
+                        </tr>
+                        <tr>
+                          <td align="right" className="align-middle">
+                            Resume:
+                          </td>
+                          <td className="align-middle">
+                            <a
+                              href="https://drive.google.com/drive/folders/16fyPdaD4kF32LHCfp5KjyuMofex6JAOo?usp=sharing"
+                              className="custom-link"
+                              target="_blank"
+                              rel="noreferrer"
+                            >
+                              <u>Click Here to check my Resume</u>
+                            </a>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td></td>
+                          <td></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
